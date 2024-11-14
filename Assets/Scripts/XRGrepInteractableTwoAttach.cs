367412f0ffb33b46883afbeec2efc32bdcb8f2ac
c55702980 <c55702980@naver.com>
2024-11-14 13:19:14 +0900

    오류 수정

--- Assets/Scripts/XRGrepInteractableTwoAttach.cs
+++ Assets/Scripts/XRGrepInteractableTwoAttach.cs
@@ -13,7 +13,7 @@ namespace MyVrSample
         public Transform leftAttachTranfrom;
         public Transform rightAttachTranfrom;
         #endregion
-        protected override void OnSelectEntered(SelectEnterEventArgs args)
+        protected override void OnSelectEntering(SelectEnterEventArgs args)
         {
             //두개의 Attach Point를 잡는 손에 따라 구분해서 적용
             if(args.interactorObject.transform.CompareTag("LeftHand"))
